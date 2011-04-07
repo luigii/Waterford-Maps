237bfd29918df59013c5d562d78a3a20f7ce2742
Conall O Maitiu <conall.o.maitiu@gmail.com>
2011-04-07 09:49:55 +0100

    Alertdialogs to get information about sites now work

--- src/snowflake/waterford/maps1/PumpMapOne.java
+++ src/snowflake/waterford/maps1/PumpMapOne.java
@@ -1,8 +1,6 @@
 package snowflake.waterford.maps1;
 
 import java.util.List;
-
-import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import com.google.android.maps.*;
@@ -11,8 +9,7 @@ public class PumpMapOne extends MapActivity {
 	List<Overlay> mapOverlays;
 	Drawable green, yellow, red;
 	StatusOverlay greenOverlay, yellowOverlay, redOverlay;
-	Context context;
-	
+		
     @Override
     protected boolean isRouteDisplayed() {
         return false;
@@ -28,19 +25,19 @@ public class PumpMapOne extends MapActivity {
         green = this.getResources().getDrawable(R.drawable.green_dot_small);
         
         red = this.getResources().getDrawable(R.drawable.red_dot_small);
-        greenOverlay = new StatusOverlay(green, context);
+        greenOverlay = new StatusOverlay(green, this);
         GeoPoint point = new GeoPoint(52256667, -7129167);
         OverlayItem pointmarker = new OverlayItem(point, "Pumphouse 1", "this is a sample");
         
         
         yellow = this.getResources().getDrawable(R.drawable.yellow_dot_small);
-        yellowOverlay = new StatusOverlay(yellow, context);
+        yellowOverlay = new StatusOverlay(yellow, this);
         GeoPoint point2 = new GeoPoint(52257372, -7128673);
         OverlayItem pointmarker2 = new OverlayItem(point2, "Pumphouse 2", "More Sample text");
         
         
         red = this.getResources().getDrawable(R.drawable.red_dot_small);
-        redOverlay = new StatusOverlay(red, context);
+        redOverlay = new StatusOverlay(red, this);
         GeoPoint point3 = new GeoPoint(52267372, -7129673);
         OverlayItem pointmarker3 = new OverlayItem(point3, "Pumphouse 3", "this might work");
         
--- src/snowflake/waterford/maps1/StatusOverlay.java
+++ src/snowflake/waterford/maps1/StatusOverlay.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 
 import com.google.android.maps.ItemizedOverlay;
@@ -12,38 +13,48 @@ import com.google.android.maps.OverlayItem;
 public class StatusOverlay extends ItemizedOverlay<OverlayItem>{
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	Context mContext;
-	
-	
+
+
 	public StatusOverlay(Drawable defaultMarker, Context context) {
 		super(boundCenter(defaultMarker));
 		mOverlays = new ArrayList<OverlayItem>();
-        populate();
+		populate();
 		mContext = context;
 	}
 
 	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+		mOverlays.add(overlay);
+		populate();
 	}
-	
+
 	@Override
 	protected OverlayItem createItem(int i) {
-		  return mOverlays.get(i);
+		return mOverlays.get(i);
 	}
 
 	@Override
 	public int size() {
 		return mOverlays.size();
 	}
-	
+
 	@Override
 	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
-	  dialog.show();
-	  return true;
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(mContext);
+		dialogbuilder.setTitle(item.getTitle())
+		.setCancelable(true)
+		.setMessage(item.getSnippet())
+		.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.cancel();
+			}
+		})
+		.create()
+		.show();
+		return true;
 	}
 
+	//void onCreateDialog(){
+		//
+	//}
 }
